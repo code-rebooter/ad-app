b2223cce00487e9cd92f3a9b85b73f96e6e18523
CoderZeng <chienzy@yeah.net>
2025-07-30 20:07:53 +0800

    hq001广告实现

--- app/src/hq001/java/com/smart/android/ad_app/AdManagerImpl.kt
+++ app/src/hq001/java/com/smart/android/ad_app/AdManagerImpl.kt
@@ -1,18 +1,48 @@
 package com.smart.android.ad_app
 
+import android.content.Context
 import android.view.View
+import android.view.ViewGroup
+import com.tcl.ff.component.vastad.Ad
+import com.tcl.ff.component.vastad.Controller
+import com.tcl.ff.component.vastad.Initialization
+import com.tcl.ff.component.vastad.core.callbacks.LazyLoaderAdListener
 
 object AdManagerImpl : IAdManager {
     override fun init() {
         //广告初始化
         println("hq001的广告初始化")
+        Initialization.init(appContext)
     }
 
     override fun showAd(flRoot: View) {
         //展示广告
         println("hq001的广告展示")
+
+        Ad.get().begin(appContext)
+            .lazyLoad()
+            .listen(object : LazyLoaderAdListener {
+                override fun onAdLoaded(controller: Controller) {
+                    controller.start(flRoot as ViewGroup)
+                }
+
+                override fun onAdFinished() {
+                    flRoot.visibility = View.GONE
+                }
+
+                override fun onAdError() {
+                    flRoot.visibility = View.GONE
+                }
+
+                override fun onContainerSizeError() {
+                    flRoot.visibility = View.GONE
+                }
+            })
+            .start()
     }
 
-
-
 }
+
+
+
+

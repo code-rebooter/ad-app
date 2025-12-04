3d49ddb9e75efee8b64bd298ba63dee22b4cd9e7
CoderZeng <chienzy@yeah.net>
2025-12-04 18:09:18 +0800

    改为5分钟请求一次

--- app/src/hq006/java/com/smart/android/ad_app/ScheduleManagerImpl.kt
+++ app/src/hq006/java/com/smart/android/ad_app/ScheduleManagerImpl.kt
@@ -3,7 +3,7 @@ package com.smart.android.ad_app
 object ScheduleManagerImpl : IScheduleManager {
     override fun handlerInitialDelayTime(): Long  = 3500
 
-    override fun handlerScheduleTime(): Long  = 50
+    override fun handlerScheduleTime(): Long  = 5*60
 
 
 }
